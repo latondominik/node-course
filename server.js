65d76a35088331404db222c29454f81f3566ab9f
Dominik <latondominik@gmail.com>
2018-11-26 18:57:00 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+const fs = require('fs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.use((req,res,next)=>{
+var now = new Date().toString();
+var log = `${now}: ${req.method}: ${req.url}`;
+console.log(log);
+fs.appendFile('server.log', log + '\n', (err)=>{
+    if(err){
+        console.log('Uneble to append to server.log');
+    }
+});
+next();
+});
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+
+});
+
+
+
+app.get('/', (req, res)=>{
+    res.render('home.hbs', { 
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Halo kurwa'
+    });
+});
+app.get('/about', (req, res)=>{
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+     });
+});
+
+app.get('/bad', (req, res)=>{
+     res.send({
+         errorMessage: 'Unable to handle request',
+         
+     });
+});
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000');
+});
